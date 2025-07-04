d94944410bc42c48a697dca0e648e8b4432a7b3d
haiphan <hai.phan@zoho.com>
2025-07-04 19:09:35 +0300

    Update lc2040

--- problems/lc2040.go
+++ problems/lc2040.go
@@ -1,20 +1,20 @@
 package problems
 
-import "slices"
+import (
+	"sort"
+)
 
-func splitBySign(nums []int) ([]int, []int) {
-	N := len(nums)
-	pos := make([]int, 0, N)
-	neg := make([]int, 0, N)
-	for _, x := range nums {
-		if x < 0 {
-			neg = append(neg, -x)
-		} else {
-			pos = append(pos, x)
-		}
+const MAXP int = 1e10
+
+func revAndAbs(A []int) []int {
+	N := len(A)
+	for i := range N {
+		A[i] = -A[i]
 	}
-	slices.Reverse(neg)
-	return neg, pos
+	for l, r := 0, N-1; l < r; l, r = l+1, r-1 {
+		A[l], A[r] = A[r], A[l]
+	}
+	return A
 }
 
 func countLte(A []int, B []int, m int) int {
@@ -24,25 +24,29 @@ func countLte(A []int, B []int, m int) int {
 		for bi >= 0 && (B[bi]*a) > m {
 			bi--
 		}
+		if bi < 0 {
+			return cnt
+		}
 		cnt += (bi + 1)
 	}
 	return cnt
 }
 
 func kthSmallestProduct(nums1 []int, nums2 []int, k int64) int64 {
-	A1, A2 := splitBySign(nums1)
-	B1, B2 := splitBySign(nums2)
+	zA := sort.SearchInts(nums1, 0)
+	zB := sort.SearchInts(nums2, 0)
+	A1, A2 := revAndAbs(nums1[:zA]), nums1[zA:]
+	B1, B2 := revAndAbs(nums2[:zB]), nums2[zB:]
 	negCnt := len(A1)*len(B2) + len(A2)*len(B1)
 	sign := 1
-	ki := int(k)
-	if ki > negCnt {
-		ki -= negCnt
-	} else {
-		ki = negCnt - ki + 1
+	k32 := int(k)
+	ki := k32 - negCnt
+	if k32 <= negCnt {
+		ki = negCnt - k32 + 1
 		sign = -1
 		B1, B2 = B2, B1
 	}
-	l, r := 0, int(1e10)
+	l, r := 0, MAXP
 	for l < r {
 		m := (l + r) / 2
 		if countLte(A1, B1, m)+countLte(A2, B2, m) >= ki {
