6999396bb52b4a7b5b307fbce062331ef442c55a
haiphan <hai.phan@zoho.com>
2025-08-06 00:15:16 +0300

    Update lc2561

--- problems/lc2561.go
+++ problems/lc2561.go
@@ -2,36 +2,59 @@ package problems
 
 import "sort"
 
-func minCost(basket1 []int, basket2 []int) int64 {
-	N := len(basket1)
-	cm := make(map[int]int, N*2)
-	for _, x := range basket1 {
-		cm[x]++
-	}
-	for _, x := range basket2 {
-		cm[x]--
-	}
-	minv := basket1[0]
-	cand := make([]int, 0, N)
-	for k, v := range cm {
-		minv = min(minv, k)
-		if v < 0 {
-			v = -v
+func minCost(b1, b2 []int) int64 {
+	n := len(b1)
+	i, j := 0, 0
+	count := make([]int, n)
+	cost := make([]int, n)
+
+	sort.Ints(b1)
+	sort.Ints(b2)
+	// swapPos is the current position in cost and count
+	// swapCnt is the total number of groups that can be formed
+	swapPos, swapCnt := 0, 0
+	minCost := min(b1[0], b2[0]) << 1
+
+	for i < n || j < n {
+		v := 0
+		cnt := 0
+		if i < n && j < n && b1[i] == b2[j] {
+			i++
+			j++
+			continue
+		} else if i < n && (j == n || b1[i] < b2[j]) {
+			v = b1[i]
+			for i < n && b1[i] == v {
+				cnt++
+				i++
+			}
+		} else if j < n && (i == n || b2[j] < b1[i]) {
+			v = b2[j]
+			for j < n && b2[j] == v {
+				cnt++
+				j++
+			}
 		}
-		if v%2 == 1 {
+		if cnt&1 != 0 {
 			return -1
 		}
-		v /= 2
-		for i := 0; i < v; i++ {
-			cand = append(cand, k)
-		}
+		cost[swapPos] = v
+		count[swapPos] = cnt >> 1
+		swapCnt += count[swapPos]
+		swapPos++
 	}
-	sort.Ints(cand)
-	UB := len(cand) / 2
-	bc := minv * 2
-	cost := 0
-	for i := range UB {
-		cost += min(bc, cand[i])
+
+	if swapCnt&1 != 0 {
+		return -1
 	}
-	return int64(cost)
+	swapCnt >>= 1
+	swapPos = 0
+	res := 0
+	for swapCnt > 0 {
+		used := min(count[swapPos], swapCnt)
+		res += min(minCost, cost[swapPos]) * used
+		swapCnt -= used
+		swapPos++
+	}
+	return int64(res)
 }
