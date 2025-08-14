9c969622c8de247133e99262629145efcf1561d2
haiphan <hai.phan@zoho.com>
2025-08-14 14:00:29 +0300

    Update lc2264

--- problems/lc2264.go
+++ problems/lc2264.go
@@ -6,7 +6,7 @@ var cand []string = []string{"999", "888", "777", "666", "555", "444", "333", "2
 
 func largestGoodInteger(num string) string {
 	for _, c := range cand {
-		if strings.Index(num, c) >= 0 {
+		if strings.Contains(num, c) {
 			return c
 		}
 	}
