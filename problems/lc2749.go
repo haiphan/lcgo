bd82af089e2895db1692e6d5cf370bcc4b55cf36
haiphan <hai.phan@zoho.com>
2025-09-05 10:48:18 +0300

    lc2749: add solution and test

---
+++ problems/lc2749.go
@@ -0,0 +1,19 @@
+package problems
+
+import "math/bits"
+
+func makeTheIntegerZero(num1 int, num2 int) int {
+	if num2 == 0 {
+		return bits.OnesCount64(uint64(num1))
+	}
+	for k := 1; k <= 60; k++ {
+		x := num1 - k*num2
+		if x < k {
+			return -1
+		}
+		if k >= bits.OnesCount64(uint64(x)) {
+			return k
+		}
+	}
+	return -1
+}
