ad2613aa87316bd4700a048bf9486bb5cf266150
haiphan <hai.phan@zoho.com>
2025-09-06 22:06:45 +0300

    Use bits op to init l0, step

--- problems/lc3495.go
+++ problems/lc3495.go
@@ -1,14 +1,14 @@
 package problems
 
+import "math/bits"
+
 func minOperationsQueries(queries [][]int) int64 {
 	ans := 0
 	for _, q := range queries {
 		l, r := q[0], q[1]
-		l0, step := 1, 1
-		for (l0 << 2) <= l {
-			l0 <<= 2
-			step++
-		}
+		pow := (bits.Len64(uint64(l)) - 1) / 2
+		l0 := 1 << (2 * pow)
+		step := pow + 1
 		cnt := 0
 		for ; l0 <= r; l0, step = 4*l0, step+1 {
 			r0 := (l0 << 2) - 1
