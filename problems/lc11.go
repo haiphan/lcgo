f4ddc64f2450dd86240c12f092861293980c0cf7
haiphan <hai.phan@zoho.com>
2025-10-04 07:23:10 +0300

    Update max area calc

--- problems/lc11.go
+++ problems/lc11.go
@@ -5,12 +5,15 @@ func maxArea(height []int) int {
 	l, r := 0, N-1
 	res := 0
 	for l < r {
-		res = max(res, (r-l)*min(height[l], height[r]))
+		v := 0
 		if height[l] <= height[r] {
+			v = height[l] * (r - l)
 			l++
 		} else {
+			v = height[r] * (r - l)
 			r--
 		}
+		res = max(res, v)
 	}
 	return res
 }
