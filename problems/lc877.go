657c59b7300f65c73562776f725a789ddc19d2b4
haiphan <hai.phan@zoho.com>
2025-10-08 00:56:50 +0300

    lc877 optimize space

--- problems/lc877.go
+++ problems/lc877.go
@@ -2,13 +2,13 @@ package problems
 
 func stoneGame(piles []int) bool {
 	N := len(piles)
-	dp := make([][]int, N)
-	for i := N - 1; i >= 0; i-- {
-		dp[i] = make([]int, N)
-		dp[i][i] = piles[i]
-		for j := i + 1; j < N; j++ {
-			dp[i][j] = max(piles[i]-dp[i+1][j], piles[j]-dp[i][j-1])
+	dp := make([]int, N)
+	copy(dp, piles)
+	for d := 1; d < N; d++ {
+		for i := 0; i+d < N; i++ {
+			// take i or i+d
+			dp[i] = max(piles[i]-dp[i+1], piles[i+d]-dp[i])
 		}
 	}
-	return dp[0][N-1] > 0
+	return dp[0] > 0
 }
