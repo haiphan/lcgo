ba746508e7f9fcc74e9bbf82e6274f765061f1f3
haiphan <hai.phan@zoho.com>
2025-10-09 01:24:34 +0300

    Add comments

--- problems/lc877.go
+++ problems/lc877.go
@@ -6,7 +6,9 @@ func stoneGame(piles []int) bool {
 	copy(dp, piles)
 	for d := 1; d < N; d++ {
 		for i := 0; i+d < N; i++ {
-			// take i or i+d
+			// dp[i] is the max score difference can get from piles[i..i+d]
+			// if take i, opponent can get dp[i+1], so net is piles[i]-dp[i+1]
+			// if take i+d, opponent can get dp[i], so net is piles[i+d]-dp[i]
 			dp[i] = max(piles[i]-dp[i+1], piles[i+d]-dp[i])
 		}
 	}
