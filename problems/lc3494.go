7868484e0cf9db2df817b7ead7cda14e42dbd518
haiphan <hai.phan@zoho.com>
2025-10-12 01:30:00 +0300

    lc3494 Update prefix init

--- problems/lc3494.go
+++ problems/lc3494.go
@@ -3,16 +3,19 @@ package problems
 func minTime(skill []int, mana []int) int64 {
 	n, m := len(skill), len(mana)
 	prefix_skills := make([]int, n)
+	prefix_skills[0] = skill[0]
 	for i := 1; i < n; i++ {
 		prefix_skills[i] = prefix_skills[i-1] + skill[i]
 	}
-	total_time := skill[0] * mana[0]
+	total_time := 0
 
 	for j := 1; j < m; j++ {
-		max_time := skill[0] * mana[j]
-		for i := 1; i < n; i++ {
-			diff_time := prefix_skills[i]*mana[j-1] - prefix_skills[i-1]*mana[j]
-			max_time = max(max_time, diff_time)
+		max_time := 0
+		cur, prev := mana[j], mana[j-1]
+		e := 0
+		for i := range n {
+			max_time = max(max_time, prefix_skills[i]*prev-e)
+			e = prefix_skills[i] * cur
 		}
 		total_time += max_time
 	}
