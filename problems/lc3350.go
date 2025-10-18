376484601d9e2c8264ecf18fb981a3ab3cbdc743
haiphan <hai.phan@zoho.com>
2025-10-18 10:39:29 +0300

    Refactor minmax logic

--- problems/lc3350.go
+++ problems/lc3350.go
@@ -2,16 +2,15 @@ package problems
 
 func maxIncreasingSubarrays(nums []int) int {
 	n := len(nums)
-	cur, prev := 1, 0
+	cur, prev := 0, 0
 	ans := 1
-	for i := 1; i < n; i++ {
-		if nums[i] > nums[i-1] {
-			cur++
-		} else {
+	for i, v := range nums {
+		cur++
+		if i+1 == n || nums[i+1] <= v {
+			ans = max(ans, cur>>1, min(cur, prev))
 			prev = cur
-			cur = 1
+			cur = 0
 		}
-		ans = max(ans, cur>>1, min(cur, prev))
 	}
 	return ans
 }
--- problems/lc3397.go
+++ problems/lc3397.go
@@ -7,8 +7,9 @@ func maxDistinctElements(nums []int, k int) int {
 	minv := nums[0] - k - 1
 	cnt := 0
 	for _, x := range nums {
-		if x+k > minv {
-			minv = max(minv+1, x-k)
+		nxt := min(max(minv+1, x-k), x+k)
+		if nxt > minv {
+			minv = nxt
 			cnt++
 		}
 	}
