0992f20eb00e0c742d944bc96724467f7a186fe9
haiphan <hai.phan@zoho.com>
2025-10-22 10:49:37 +0300

    lc3347 Use array instead of map for counts

--- problems/lc3347.go
+++ problems/lc3347.go
@@ -4,22 +4,34 @@ import "sort"
 
 func maxFrequency(nums []int, k int, numOperations int) int {
 	L := len(nums)
-	cm := make(map[int]int, len(nums))
+	cm := make([]int, L)
 	n := numOperations
 	sort.Ints(nums)
-	for _, x := range nums {
-		cm[x]++
+	l, r := 0, 0
+	for r < L {
+		for r < L && nums[r] == nums[l] {
+			r++
+		}
+		cnt := r - l
+		for l < r {
+			cm[l] = cnt
+			l++
+		}
 	}
-	l, r, res := 0, 0, 0
-	for _, v := range nums {
+	l, r = 0, 0
+	res := 0
+	for i, v := range nums {
 		for nums[l]+k < v {
 			l++
 		}
-		for r+1 < L && v+k >= nums[r+1] {
+		for r < L && v+k >= nums[r] {
 			r++
 		}
-		cv := cm[v]
-		cnt := r - l + 1
+		cnt := r - l
+		if cnt <= res {
+			continue
+		}
+		cv := cm[i]
 		change := min(n, cnt-cv)
 		res = max(res, cv+change)
 	}
@@ -27,11 +39,12 @@ func maxFrequency(nums []int, k int, numOperations int) int {
 		return res
 	}
 	l, r = 0, 0
+	tk := (k << 1)
 	for ; r < L; r++ {
-		for nums[l]+2*k < nums[r] {
+		for (r-l+1) > n || nums[l]+tk < nums[r] {
 			l++
 		}
-		res = max(res, min(n, r-l+1))
+		res = max(res, r-l+1)
 	}
 	return res
 }
