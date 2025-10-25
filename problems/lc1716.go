2f7f904ea667eeb12aa65ae8dde21f1b5149ae88
haiphan <hai.phan@zoho.com>
2025-10-26 01:38:43 +0300

    lc1716 use direct formula

--- problems/lc1716.go
+++ problems/lc1716.go
@@ -3,9 +3,7 @@ package problems
 const WEEK_EARN int = 28
 
 func totalMoney(n int) int {
-	if n <= 7 {
-		return (n * (n + 1)) >> 1
-	}
-	n -= 7
-	return WEEK_EARN + n + totalMoney(n)
+	y := n % 7
+	k := (n - y) / 7
+	return k*WEEK_EARN + k*n - ((7 * k * (k + 1)) >> 1) + ((y * (y + 1)) >> 1)
 }
