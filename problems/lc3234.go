5a834947d4e28348f470b481c450e4ce406cce10
haiphan <hai.phan@zoho.com>
2025-11-16 00:36:54 +0200

    lc3234 Use global arr

--- problems/lc3234.go
+++ problems/lc3234.go
@@ -1,20 +1,23 @@
 package problems
 
+var PZ [40000]int
+
 func numberOfSubstrings(s string) int {
 	ans := 0
 	ones := 0
-	pz := []int{-1}
+	PZ[0] = -1
+	zl := 1
 	for i, c := range s {
 		if c == '0' {
-			pz = append(pz, i)
+			PZ[zl] = i
+			zl++
 		} else {
 			ones++
-			ans += i - pz[len(pz)-1]
+			ans += i - PZ[zl-1]
 		}
-		lz := len(pz)
-		for j := lz - 1; j >= 1; j-- {
-			cur, prev := pz[j], pz[j-1]
-			c0 := lz - j
+		for j := zl - 1; j >= 1; j-- {
+			cur, prev := PZ[j], PZ[j-1]
+			c0 := zl - j
 			c0Sq := c0 * c0
 			if c0Sq > ones {
 				break
