30207ee9a95329287e6e6d8331539fc91b9cfa62
haiphan <hai.phan@zoho.com>
2025-11-18 10:23:43 +0200

    lc717 Simplify i step

--- problems/lc717.go
+++ problems/lc717.go
@@ -4,11 +4,7 @@ func isOneBitCharacter(bits []int) bool {
 	n := len(bits)
 	i, end := 0, n-2
 	for i <= end {
-		if bits[i] == 1 {
-			i += 2
-		} else {
-			i++
-		}
+		i += 1 + bits[i]
 	}
 	return i == n-1
 }
