9a8fb797725c6e95838f6d2e7ea29183309874c6
haiphan <hai.phan@zoho.com>
2025-11-28 01:50:44 +0200

    Add SerializeTree util

--- utils/TreeNode.go
+++ utils/TreeNode.go
@@ -1,7 +1,25 @@
 package utils
 
+import "fmt"
+
+const NullMarker = "#"
+const Separator = ","
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
+}
+
+func SerializeTree(root *TreeNode) string {
+	if root == nil {
+		return NullMarker
+	}
+
+	// Root -> Left -> Right
+	leftSerialized := SerializeTree(root.Left)
+	rightSerialized := SerializeTree(root.Right)
+
+	// Combine components into a comma-separated string
+	return fmt.Sprintf("%d%s%s%s%s", root.Val, Separator, leftSerialized, Separator, rightSerialized)
 }
