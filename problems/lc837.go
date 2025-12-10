b82d54279aba03d3aaf0d286c6bc130bd98d327a
haiphan <hai.phan@zoho.com>
2025-12-10 05:45:03 +0200

    lc837 add comments

--- problems/lc837.go
+++ problems/lc837.go
@@ -5,17 +5,22 @@ func new21Game(n int, k int, maxPts int) float64 {
 		return 1
 	}
 	mp := float64(maxPts)
+	// dp[i]: probability to get points i
 	dp := make([]float64, n+1)
 	dp[0] = 1.0
+	// wsum: window sum
 	res, wsum := 0.0, 1.0
 	for i := 1; i <= n; i++ {
 		dp[i] = wsum / mp
 		if i < k {
+			// still drawing
 			wsum += dp[i]
 		} else {
+			// stop drawing, result accumulate
 			res += dp[i]
 		}
 		if i >= maxPts {
+			// shrink the window
 			wsum -= dp[i-maxPts]
 		}
 	}
