a29c42a6bb75ec3e999839f13858aadf34ec96e0
haiphan <hai.phan@zoho.com>
2025-12-25 15:22:19 +0200

    lc2054 line sweep with radix sort

--- problems/lc2054.go
+++ problems/lc2054.go
@@ -2,34 +2,73 @@ package problems
 
 import "sort"
 
+var EVCACHE, EVBUF [200000]int
+
+func radixSortMSBOnly(arr []int) {
+	n := len(arr)
+	buffer := EVBUF[:n]
+
+	const bitsPerPass = 11
+	const bucketSize = 1 << bitsPerPass
+	const mask = bucketSize - 1
+	swap := false
+	// Start at bit 20, end at bit 51 (31 bits total)
+	for shift := uint(20); shift < 51; shift += bitsPerPass {
+		var count [bucketSize]int
+
+		for _, v := range arr {
+			count[(v>>shift)&mask]++
+		}
+
+		pos := 0
+		for i := 0; i < bucketSize; i++ {
+			tmp := count[i]
+			count[i] = pos
+			pos += tmp
+		}
+
+		for _, v := range arr {
+			idx := (v >> shift) & mask
+			buffer[count[idx]] = v
+			count[idx]++
+		}
+
+		arr, buffer = buffer, arr
+		swap = !swap
+	}
+	if swap {
+		copy(buffer, arr)
+	}
+}
+
+func customSort(arr []int) {
+	if len(arr) < 1<<9 {
+		sort.Ints(arr)
+	} else {
+		radixSortMSBOnly(arr)
+	}
+}
+
 func maxTwoEvents(events [][]int) int {
 	n := len(events)
-	sort.Slice(events, func(i, j int) bool {
-		return events[i][0] < events[j][0]
-	})
-	suf := make([]int, n)
-	suf[n-1] = events[n-1][2]
-	for i := n - 2; i >= 0; i-- {
-		suf[i] = max(suf[i+1], events[i][2])
+	m := n << 1
+	arr := EVCACHE[:m]
+	eMask := 1 << 20
+	vMask := eMask - 1
+	for i := range events {
+		s, e, v := events[i][0], events[i][1], events[i][2]
+		arr[i*2] = s<<21 | v
+		arr[i*2+1] = e<<21 | eMask | v
 	}
-	ans := 0
-	for i, e := range events {
-		cur := e[2]
-		j := -1
-		l, r := i+1, n-1
-		for l <= r {
-			m := (l + r) >> 1
-			if events[m][0] > e[1] {
-				j = m
-				r = m - 1
-			} else {
-				l = m + 1
-			}
+	customSort(arr)
+	ans, maxv := 0, 0
+	for _, t := range arr {
+		v := t & vMask
+		if (t & eMask) == 0 {
+			ans = max(ans, maxv+v)
+		} else {
+			maxv = max(maxv, v)
 		}
-		if j != -1 {
-			cur += suf[j]
-		}
-		ans = max(ans, cur)
 	}
 	return ans
 }
